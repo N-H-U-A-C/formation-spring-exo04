3e0e358f6acbf21baab14e8007000b9961f49094
cedric <cedric.belot.dev@gmail.com>
2024-07-23 12:40:23 +0200

    initial commit

---
+++ src/main/java/dev/cb/FormationSpringExo04Application.java
@@ -0,0 +1,13 @@
+package dev.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationSpringExo04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationSpringExo04Application.class, args);
+	}
+
+}
